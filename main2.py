05ac3a12a3020c39baf99c857d59fdd69ae6d866
programminwithdhairya <105724103+programminwithdhairya@users.noreply.github.com>
2022-05-28 22:31:58 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,29 @@
+# Guessing game in Python 
+import random 
+randnum  = random.randint(1,100)
+# print(randnum)
+print("You will get only 10 Chances to Guess the number")
+
+def Guess():
+    guesses = 1
+    while True:
+        user_guess = int(input("Enter Your Guess:\n"))
+        if user_guess == randnum:
+            print(f"you won!! , You took {guesses} gusses")
+            exit()
+            
+        else:
+            if user_guess > randnum : 
+                print("You are guessing a big number guess a Small number")
+                guesses  = guesses + 1
+
+            else:
+                print("You are guessing a small number Guess a big number")
+                guesses  = guesses + 1
+        if guesses == 10:
+                print(f"Game Over.... you took {guesses} Guesses")
+                break
+
+
+game = Guess()
+print(game)
