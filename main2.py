9a326ca5f9d80552507cdcbc2446b2561e680950
programminwithdhairya <105724103+programminwithdhairya@users.noreply.github.com>
2022-05-28 22:37:00 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@
 import random 
 randnum  = random.randint(1,100)
 # print(randnum)
-print("You will get only 10 Chances to Guess the number")
+print("You will get only 5 Chances to Guess the number")
 
 def Guess():
     guesses = 1
@@ -20,7 +20,7 @@ def Guess():
             else:
                 print("You are guessing a small number Guess a big number")
                 guesses  = guesses + 1
-        if guesses == 10:
+        if guesses == 5:
                 print(f"Game Over.... you took {guesses} Guesses")
                 break
 
